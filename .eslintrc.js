996c4ea69583024e98e34c1a67bac60fb91c5139
Diego Fernandes <diego.schell.f@gmail.com>
2019-07-05 16:05:50 -0300

    12 - Reactotron + Redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
         extensions: ['.jsx', '.js'],
       },
     ],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'no-console': ["error", { allow: ["tron"] }]
   }
 };
